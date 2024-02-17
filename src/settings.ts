9c4b45b58a7a6f8749f1b008529b5d51eff12b24
Jorge Ramirez <jorge.a.ramirez@mac.com>
2024-02-16 20:57:10 -0500

    Updated settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = "RokuTV";
+export const PLATFORM_NAME = "HomebridgeRokuTVs";
 
 /**
  * This must match the name of your plugin as defined the package.json
